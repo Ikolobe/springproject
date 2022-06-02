21bfc79bc5a12e6dba100d7b24f743cee874fd66
shand <shand@DESKTOP-HD7VKMH>
2022-06-01 21:30:04 -0300

    More corrections on the code for better development.

--- src/main/java/com/springproject/restapi/ApplicationContextLoad.java
+++ src/main/java/com/springproject/restapi/ApplicationContextLoad.java
@@ -11,6 +11,7 @@ public class ApplicationContextLoad implements ApplicationContextAware{
 	private static ApplicationContext applicationContext;
 	
 	
+	@SuppressWarnings("static-access")
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.applicationContext = applicationContext;
