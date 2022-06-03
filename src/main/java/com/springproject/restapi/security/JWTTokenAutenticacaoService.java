d50d51d1a4dd9cde93162aa6801a428513c28282
shand <shand@DESKTOP-HD7VKMH>
2022-06-03 08:52:43 -0300

    Cors corrections were made.

--- src/main/java/com/springproject/restapi/security/JWTTokenAutenticacaoService.java
+++ src/main/java/com/springproject/restapi/security/JWTTokenAutenticacaoService.java
@@ -48,13 +48,16 @@ public class JWTTokenAutenticacaoService {
 		/*adiciona no cabeçalho http*/
 		response.addHeader(HEADER_STRING, token);
 		
+		/*Liberando resposta para portas diferentes que usam a API*/
+		liberacaoCors(response);
+		
 		/*Escreve token como resposta no corpo http*/
 		response.getWriter().write("{\"Authorization\": \""+token+"\"}");
 		
 	}
 	
 	/*Retorn o usuário validado com o token ou caso não seja valido retorna null*/
-	public Authentication getAuthentication(HttpServletRequest request) {
+	public Authentication getAuthentication(HttpServletRequest request, HttpServletResponse response) {
 		
 		/*Pega o token enviado no cabeçalho http*/
 		String token = request.getHeader(HEADER_STRING);
@@ -84,7 +87,25 @@ public class JWTTokenAutenticacaoService {
 			}
 			
 		}
+			liberacaoCors(response);
 			return null;
+		
+	}
+
+	private void liberacaoCors(HttpServletResponse response) {
+
+		if(response.getHeader("Access-Control-Allow-Origin") == null) {
+			response.addHeader("Access-Control-Allow-Origin", "*");
+		}
+		if (response.getHeader("Access-Control-Allow-Headers") == null) {
+			response.addHeader("Access-Control-Allow-Headers", "*");
+		}
+		if (response.getHeader("Access-Control-Request-Headers") == null) {
+			response.addHeader("Access-Control-Request-Headers", "*");
+		}
+		if (response.getHeader("Access-Control-Allow-Methods") == null) {
+			response.addHeader("Access-Control-Allow-Methods", "*");
+		}
 		
 	}	
 	
